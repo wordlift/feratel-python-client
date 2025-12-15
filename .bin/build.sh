df945b3dc62b9d96b82fece8b861d32a48898eb9
David Riccitelli <david@wordlift.io>
2025-12-15 16:42:12 +0100

    initial commit

---
+++ .bin/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#npx @openapitools/openapi-generator-cli generate \
+#  -i ./openapi/openapi.json \
+#  -g python \
+#  -c config.json \
+#  --package-name 'feratel_client' \
+#  --library 'asyncio' \
+#  --additional-properties=pythonVersion=3.11
+
+docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli:v7.17.0 generate \
+  -i /local/openapi/openapi.json \
+  -g python \
+  -c /local/config.json \
+  --package-name 'feratel_client' \
+  --library 'asyncio' \
+  --additional-properties=pythonVersion=3.11 \
+  -o /local/feratel_client
